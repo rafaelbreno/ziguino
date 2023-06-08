8b9b56d92bf193aca6c5ec5e18ce94ed4650534c
Rafael Breno <rafael_breno@outlook.com>
2023-06-08 19:09:58 -0300

    feat: Add comptime to start arduino

--- zig/src/start.zig
+++ zig/src/start.zig
@@ -1,8 +1,23 @@
 const std = @import("std");
 const builtin = std.builtin;
+const lib = @import("lib/lib.zig");
+const gpio = lib.gpio;
+const delay_ms = lib.delay.delay_ms;
+
+comptime {
+    var asm_str: []const u8 = ".section .vectors\njmp _start\n";
+    asm (asm_str);
+}
 
 pub export fn _start() callconv(.Naked) noreturn {
-    while (true) {}
+    gpio.init(5, .out);
+
+    while (true) {
+        gpio.toggle(5);
+        delay_ms(1000);
+    }
+
+    unreachable;
 }
 
 pub fn panic(_: []const u8, _: ?*builtin.StackTrace, _: ?usize) noreturn {
